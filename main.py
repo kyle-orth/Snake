4d40812ae9ba6857da32e605f107eb34423da22e
kyle-orth <kyle.orth23@gmail.com>
2022-01-16 00:10:47 -0700

    Input management, structure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
 Basic game: Snake. Run using PyGame.
+https://github.com/kyle-orth/snake.git
 """
 import os
 import random
@@ -26,11 +27,13 @@ class Snake:
     def __init__(self, color='green'):
         self.color = color
         self.pos = (0, 0)
-        self.direction = (0, 1)
+        self.direction = (0, 0)
         self.coords = []
         self.length = 4
 
     def move(self):
+        if self.direction == (0, 0):
+            return
         new_pos = (self.pos[0] + self.direction[0], self.pos[1] + self.direction[1])
         self.pos = new_pos
         self.coords.append(self.pos)
@@ -48,9 +51,9 @@ class Game:
         pg.init()
         self.window = pg.display.set_mode(windowSize[0], windowSize[1])
         pg.display.set_caption("Snake")
+        self.clock = pg.time.Clock()
 
         self.score = 0
-
         self.snake = self.setup_snake()
         self.apple = Apple()
         self.border = pg.Rect(tileSize, tileSize, cols*tileSize, rows*tileSize)
@@ -65,19 +68,99 @@ class Game:
         snake.coords.append([6, 8])
         return snake
 
-    def apple_collision(self):
+    def collisions(self):
+        # Apple collision
         if self.snake.pos == self.apple.pos:
             self.apple = Apple()
             self.snake.length += 1
 
-    def snake_collision(self):
-        layers = self.snake.coords.count(self.snake.pos)
-        if layers != 1:
+        # Snake collision
+        if self.snake.coords.count(self.snake.pos) != 1:
             self.end()
 
-    def border_collision(self):
+        # Border collision
         if self.snake.pos[0] < 0 or self.snake.pos[0] > cols or self.snake.pos[1] < 0 or self.snake.pos[1] > rows:
             self.end()
 
-    def end(self):
+    def input(self):
+        events = pg.event.get()
+        if Input.quit(events):
+            pg.quit()
+        else:
+            new_direction = Input.direction(events, self.snake.direction)
+            if type(new_direction) == tuple:
+                self.snake.direction = new_direction
+
+    def display(self):
         pass
+
+    def update(self):
+        self.collisions()
+        self.input()
+        self.display()
+
+    def end(self):
+        shade = pg.Surface(windowSize)
+        shade.set_alpha(128)
+        self.window.blit(shade, (0, 0))
+
+
+class Input:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def direction(events, current_direction):
+        for event in events:
+            if event.type is pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    if current_direction[1] != 1:
+                        current_direction = (0, -1)
+                        break
+                elif event.key == pg.K_DOWN:
+                    if current_direction[1] != -1:
+                        current_direction = (0, 1)
+                        break
+                elif event.key == pg.K_LEFT:
+                    if current_direction[0] != 1 and current_direction != (0, 0):
+                        current_direction = (-1, 0)
+                        break
+                elif event.key == pg.K_RIGHT:
+                    if current_direction[0] != -1:
+                        current_direction = (1, 0)
+                        break
+        return current_direction
+
+    @staticmethod
+    def quit(events):
+        for event in events:
+            if event.type is pg.QUIT:
+                return True
+            elif event.type is pg.KEYDOWN:
+                key = event.key
+                if key == pg.K_ESCAPE:
+                    return True
+        return False
+
+
+class Display:
+    def __init__(self):
+        pass
+
+
+class Text:
+    def __init__(self):
+        pass
+
+
+def run():
+    game = Game()
+    clock = pg.time.Clock()
+    while True:
+        clock.tick(20)
+        game.update()
+
+
+if __name__ == "__main__":
+    pass
+    # run()
